4cd17e5983aebbd038b19e9a51283e964f0b77fb
Masato Ikeda <masato.ikeda@gmail.com>
2017-02-10 00:02:01 +0900

    Remake Request to Hash-based

--- lib/erbc/request.rb
+++ lib/erbc/request.rb
@@ -1,13 +1,41 @@
 module Erbc
   class Request
-    attr_accessor :erb, :config, :output, :trim_mode, :vars
+    attr_reader :env
 
-    def initialize
-      @erb        = nil
-      @config     = nil
-      @output     = nil
-      @trim_mode  = "-".freeze
-      @vars       = {}
+    def initialize(args = {})
+      @env = default.merge(symbolize_keys(args))
+    end
+
+    DEFAULT = {
+      erb:        nil,
+      config:     nil,
+      output:     nil,
+      trim_mode:  "-".freeze,
+      vars:       {},
+    }.freeze
+
+    def default
+      DEFAULT
+    end
+
+    DEFAULT.each_keys do |key|
+      class_eval <<-RUBY, __FILE__, __LINE__+1
+        def #{key}
+          env[:#{key}]
+        end
+        def #{key}=(val)
+          env[:#{key}] = val
+        end
+      RUBY
+    end
+
+    private
+
+    def symbolize_keys(hash)
+      hash.inject({}) do |ret, (key, val)|
+        ret[key.to_sym] = val
+        ret
+      end
     end
   end
 end
